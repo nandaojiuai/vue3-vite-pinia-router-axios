3c4f112b447ad6c44d4c149095a22df7f7d9e756
zpx <1164558645@qq.com>
2024-03-07 10:51:00 +0800

    element-plus组件引入，自动导入配置完毕

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>登录</div>
+  <div>登录</div>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
+//vue单文件组件识别
 import vue from '@vitejs/plugin-vue'
+//node文件组件
 import path from 'path'
-
-// https://vitejs.dev/config/
+//element-plus 按需自动导入配置
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers:[ElementPlusResolver()],
+        }),
+        Components({
+            resolvers:[ElementPlusResolver()],
+        }),
+    ],
     resolve: {
+        //可以使用@来代表src目录
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
