0d4e715f2a5b6c0c607c2791369182c3d7d5f455
zpx <1164558645@qq.com>
2024-03-08 15:10:30 +0800

    新增二维码公共组件,vite跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
+//vue单文件组件识别
 import vue from '@vitejs/plugin-vue'
+//node文件组件
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        hmr: true, //vue3 vite配置热更新不用手动刷新
+        port: 8080,
+        proxy: {
+            '/apis': {
+                target: 'https://xxxx',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/apis/, ''),
+            },
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
